4bfd94dbcb1499cddd88a7e4335065f05feba1d8
FreshEffect15 <142315233+FreshEffect15@users.noreply.github.com>
2025-03-27 09:01:13 -0300

    Arreglar modal

--- proyecto1/tareasPrueba/src/components/ui/Modal/Modal.tsx
+++ proyecto1/tareasPrueba/src/components/ui/Modal/Modal.tsx
@@ -8,79 +8,72 @@ type IModal = {
     handleCloseModal: VoidFunction
 }
 
-const intialState: ITarea = {
+const initialState: ITarea = {
     titulo: "",
     fechaInicio: "",
     fechaLimite: "",
 }
 
-export const Modal:FC<IModal> = ({handleCloseModal}) => {
-    const tareaActiva = tareaStore ((state)=> state.tareaActiva);
-    const setTareaActiva = tareaStore ((state)=> state.setTareaActiva);
+export const Modal: FC<IModal> = ({ handleCloseModal }) => {
+    const tareaActiva = tareaStore((state) => state.tareaActiva);
+    const setTareaActiva = tareaStore((state) => state.setTareaActiva);
 
+    const { crearTarea, putTareaEditar } = useTareas();
 
-    const {crearTarea, putTareaEditar} = useTareas()
+    const [formValues, setFormValues] = useState<ITarea>(initialState);
 
-    const [formValues, setFormValues] = useState <ITarea>(intialState)
-
-    useEffect (() => {
+    useEffect(() => {
         if (tareaActiva) setFormValues(tareaActiva);
-    }, []);
+    }, [tareaActiva]);
 
-    const handleChange = (
-        e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-    ) => {
-        const {name, value} = e.target;
-        setFormValues((prev) => ({... prev, [`${name}`]:value}));
+    const handleChange = (e: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
+        const { name, value } = e.target;
+        setFormValues((prev) => ({ ...prev, [name]: value }));
     };
 
     const handleSubmit = (e: FormEvent) => {
         e.preventDefault();
-        if (tareaActiva){
+        if (tareaActiva) {
             putTareaEditar(formValues);
-        }else{
-            crearTarea({ ... formValues, id: new Date().toDateString() })
+        } else {
+            crearTarea({ ...formValues, id: new Date().toDateString() });
         }
-        setTareaActiva(null)
-        handleCloseModal()
-    }
+        setTareaActiva(null);
+        handleCloseModal();
+    };
 
     return (
         <div className={styles.containerPrincipalModal}>
-            <div className={styles.countentPopUp}>
-                <div>
-                    <h3>{tareaActiva ? "Editar tarea":"Crear tarea"}</h3>
-                </div>
-                <form onSubmit={handleSubmit} className={styles.formCountent}>
-                    <div>
-                        <input 
-                            placeholder="Ingrese un titulo" 
-                            type="text" 
-                            required 
-                            onChange={handleChange}
-                            value={formValues.titulo}
-                            autoComplete="off" 
-                            name="titulo"
-                        />
-                        <input 
-                            type="date" 
-                            value={formValues.fechaInicio}
-                            required 
-                            onChange={handleChange}
-                            autoComplete="off" 
-                            name="fechaInicio"
-                        />
-                        <input 
-                            type="date" 
-                            value={formValues.fechaLimite}
-                            required 
-                            onChange={handleChange}
-                            autoComplete="off" 
-                            name="fechaLimite"
-                        />
-                    </div>
+            <div className={styles.contentPopUp}>
+                <h3>{tareaActiva ? "Editar tarea" : "Crear tarea"}</h3>
+                <form onSubmit={handleSubmit} className={styles.formContent}>
+                    <input 
+                        placeholder="Ingrese un título" 
+                        type="text" 
+                        required 
+                        onChange={handleChange}
+                        value={formValues.titulo}
+                        autoComplete="off" 
+                        name="titulo"
+                    />
+                    <input 
+                        type="date" 
+                        value={formValues.fechaInicio}
+                        required 
+                        onChange={handleChange}
+                        autoComplete="off" 
+                        name="fechaInicio"
+                    />
+                    <input 
+                        type="date" 
+                        value={formValues.fechaLimite}
+                        required 
+                        onChange={handleChange}
+                        autoComplete="off" 
+                        name="fechaLimite"
+                    />
                     <div className={styles.buttonCard}>
-                        <button onClick={handleCloseModal}>Cancelar</button>
+                        <button type="button" onClick={handleCloseModal}>Cancelar</button>
                         <button type="submit">
                             {tareaActiva ? "Editar tarea" : "Crear tarea"}
                         </button>
