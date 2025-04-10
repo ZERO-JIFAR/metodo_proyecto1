214ecd12e348745067e6e3f9073783a3a2c94a8e
FreshEffect15 <142315233+FreshEffect15@users.noreply.github.com>
2025-04-10 01:34:12 -0300

    funciones agregadas

--- src/components/ui/Modal/ModalBacklog/ModalBacklog.tsx
+++ src/components/ui/Modal/ModalBacklog/ModalBacklog.tsx
@@ -1,23 +1,46 @@
-import React, { useState } from 'react';
-import styles from './ModalBacklog.module.css';
+import React, { useEffect, useState } from "react";
+import styles from "./ModalBacklog.module.css";
+import { ITarea } from "../../../../types/ITarea";
 
 interface ModalBacklogProps {
   isOpen: boolean;
   onClose: () => void;
   onAddTask: (titulo: string, descripcion: string) => void;
+  onUpdateTask: (titulo: string, descripcion: string) => void;
+  isEditing: boolean;
+  editingTask: ITarea | null;
 }
 
-const ModalBacklog: React.FC<ModalBacklogProps> = ({ isOpen, onClose, onAddTask }) => {
-  const [titulo, setTitulo] = useState('');
-  const [descripcion, setDescripcion] = useState('');
+const ModalBacklog: React.FC<ModalBacklogProps> = ({
+  isOpen,
+  onClose,
+  onAddTask,
+  onUpdateTask,
+  isEditing,
+  editingTask,
+}) => {
+  const [titulo, setTitulo] = useState("");
+  const [descripcion, setDescripcion] = useState("");
 
-  const handleAddTask = () => {
-    if (titulo && descripcion) {
-      onAddTask(titulo, descripcion);
-      setTitulo('');
-      setDescripcion('');
+  useEffect(() => {
+    if (isEditing && editingTask) {
+      setTitulo(editingTask.titulo);
+      setDescripcion(editingTask.descripcion);
     } else {
-      alert('Por favor, completa el título y la descripción.');
+      setTitulo("");
+      setDescripcion("");
+    }
+  }, [isEditing, editingTask]);
+
+  const handleSubmit = () => {
+    if (titulo && descripcion) {
+      if (isEditing) {
+        onUpdateTask(titulo, descripcion);
+      } else {
+        onAddTask(titulo, descripcion);
+      }
+    } else {
+      alert("Por favor, completa el título y la descripción.");
     }
   };
 
@@ -26,8 +49,9 @@ const ModalBacklog: React.FC<ModalBacklogProps> = ({ isOpen, onClose, onAddTask 
   return (
     <div className={styles.modalBacklog}>
       <div className={styles.modalContentBacklog}>
-        <h2 className={styles.modalTitleBacklog}>Crear Tarea de Backlog</h2>
-
+        <h2 className={styles.modalTitleBacklog}>
+          {isEditing ? "Editar Tarea de Backlog" : "Crear Tarea de Backlog"}
+        </h2>
         <input
           className={styles.modalInputBacklog}
           type="text"
@@ -35,20 +59,18 @@ const ModalBacklog: React.FC<ModalBacklogProps> = ({ isOpen, onClose, onAddTask 
           onChange={(e) => setTitulo(e.target.value)}
           placeholder="Título"
         />
-
         <textarea
           className={styles.modalTextareaBacklog}
           value={descripcion}
           onChange={(e) => setDescripcion(e.target.value)}
           placeholder="Descripción"
         />
-
         <div className={styles.modalButtonGroup}>
           <button className={styles.modalCancelButtonBacklog} onClick={onClose}>
             Cancelar
           </button>
-          <button className={styles.modalButtonBacklog} onClick={handleAddTask}>
-            Crear Tarea
+          <button className={styles.modalButtonBacklog} onClick={handleSubmit}>
+            {isEditing ? "Guardar Cambios" : "Crear Tarea"}
           </button>
         </div>
       </div>
